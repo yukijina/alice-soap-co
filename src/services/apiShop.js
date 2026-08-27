fee45e4b871c688f8d82b753196a1d2ef719346c
yuki <38411009+yukijina@users.noreply.github.com>
2026-08-27 15:32:24 -0700

    update api_url format

--- src/services/apiShop.js
+++ src/services/apiShop.js
@@ -1,7 +1,7 @@
-const API_URL = 'https://fakestoreapi.com/products?limit=20';
+const API_URL = 'https://fakestoreapi.com/products';
 
 export async function getProducts() {
-  const res = await fetch(API_URL);
+  const res = await fetch(`${API_URL}?limit=20`);
 
   if (!res.ok) throw Error('Failed getting products');
 
