82d0b70119e8431a4c34ad18e8be0d2ef6aa554c
yuki <38411009+yukijina@users.noreply.github.com>
2026-08-26 17:23:33 -0700

    display each product, apply css

---
+++ src/features/product/ProductItem.jsx
@@ -0,0 +1,18 @@
+function ProductItem({ soap }) {
+  return (
+    <div className='bg-orange-100 rounded-2xl py-5 px-3 w-100 h-120'>
+      <div className='flex justify-center pb-12'>
+        <img
+          src={soap.image}
+          alt={soap.title}
+          className='size-60 object-contain'
+        />
+      </div>
+      <h5>{soap.title}</h5>
+      <p className='text-stone-500 pb-3'>{soap.category}</p>
+      <p className='font-semibold'>US$ {soap.price}</p>
+    </div>
+  );
+}
+
+export default ProductItem;
