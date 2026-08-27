ea0ae1ca599d071224b558830fb2049742784633
yuki <38411009+yukijina@users.noreply.github.com>
2026-08-27 15:35:58 -0700

    update loader name

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AppLayout from './layout/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 import Home from './pages/LandingPage/Home';
-import Product, { loader as itemsLoader } from './features/product/Product';
+import Products, {
+  loader as productsLoader,
+} from './features/product/Products';
 import OurStory from './pages/OurStory';
 
 const router = createBrowserRouter([
@@ -18,7 +20,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       // react router can fetch data once it goes to shop-all
-      { path: '/shop-all', element: <Product />, loader: itemsLoader },
+      { path: '/shop-all', element: <Products />, loader: productsLoader },
       { path: '/our-story', element: <OurStory /> },
     ],
   },
