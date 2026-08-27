fd65295cbdce8b26dcb3bb598ed5c5b7e8af693a
yuki <38411009+yukijina@users.noreply.github.com>
2026-08-26 17:22:52 -0700

    create loader function to get product data. Render result to ProductItem component

--- src/features/product/Product.jsx
+++ src/features/product/Product.jsx
@@ -1,9 +1,24 @@
+import { useLoaderData } from 'react-router-dom';
+import { getProducts } from '../../services/apiShop';
+import ProductItem from './ProductItem';
+
 function Product() {
+  const products = useLoaderData();
   return (
-    <div>
-      <h2>Product page</h2>
+    <div className='section-px mt-20 my-20'>
+      <h2>Shop All</h2>
+      <section className='flex flex-wrap gap-3 justify-center items-center mt-20'>
+        {products.map((product) => (
+          <ProductItem soap={product} key={product.id} />
+        ))}
+      </section>
     </div>
   );
+}
+
+export async function loader() {
+  const items = await getProducts();
+  return items;
 }
 
 export default Product;
