75ad0bd0b946c79088c835fd362febf123b5a97d
yuki <38411009+yukijina@users.noreply.github.com>
2026-08-27 16:38:25 -0700

    with useNavigator hook, add another button component to go back to the previous page

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,8 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Button({ children, to, type, onClick }) {
+  const navigate = useNavigate();
+
   const base =
     'inline-block text-sm rounded-full font-semibold tracking-wide transition-colors duration-300 focus:outline-none focus:ring focus:ring-taupe-700 focus:ring-offset-2 px-6 py-3';
 
@@ -8,6 +10,14 @@ function Button({ children, to, type, onClick }) {
     dark: base + ' bg-taupe-800 text-slate-50 hover:bg-taupe-700',
     light: base + ' border border-taupe-800 hover:bg-amber-100',
   };
+
+  if (to === '-1')
+    return (
+      <Link onClick={() => navigate(-1)} className={styles[type]}>
+        {children}
+      </Link>
+    );
+
   if (to)
     return (
       <Link to={to} className={styles[type]}>
