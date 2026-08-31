928ab0f142f568ad4c06d41b7f0840f3b1aee593
yuki <38411009+yukijina@users.noreply.github.com>
2026-08-31 16:38:27 -0700

    add table contents and display checkout summary

--- src/features/checkout/CheckoutSummary.jsx
+++ src/features/checkout/CheckoutSummary.jsx
@@ -1,7 +1,39 @@
-function CheckoutSummary() {
+function CheckoutSummary({ totalCartPrice }) {
+  console.log(totalCartPrice);
+  const shippingCost = 8;
+  const tax = 0;
+  const totalPrice = totalCartPrice + shippingCost + tax;
+
   return (
     <div>
-      <h3 className='font-instrument'>Order Summary</h3>
+      <table className='table-auto'>
+        <thead>
+          <tr>
+            <th className='text-lg pb-10'>Order Summary</th>
+          </tr>
+        </thead>
+        <tbody className='bg-orange-100'>
+          <tr>
+            <td className='px-8 py-2'>Subtotal</td>
+            <td className='px-8 py-2'>${totalCartPrice.toFixed(2)}</td>
+          </tr>
+
+          <tr>
+            <td className='px-8 py-2'>Shipping</td>
+            <td className='px-8 py-2'> ${shippingCost.toFixed(2)}</td>
+          </tr>
+
+          <tr>
+            <td className='px-8 py-2'>Estimated Tax</td>
+            <td className='px-8 py-2'>${tax.toFixed(2)}</td>
+          </tr>
+
+          <tr className='border-t border-gray-300 font-semibold'>
+            <td className='px-8 py-2'>Estimated Total</td>
+            <td className='px-8 py-2'>${totalPrice.toFixed(2)}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
