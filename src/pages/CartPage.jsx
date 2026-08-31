f8006f3ece2d39d4ef4055f9b885f199ec95c19c
yuki <38411009+yukijina@users.noreply.github.com>
2026-08-31 16:39:40 -0700

    move EmptyCart page to this page, get cart data(state) and pass it to childern

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,19 @@
+import { useSelector } from 'react-redux';
 import Cart from '../features/cart/Cart';
 import CheckoutSummary from '../features/checkout/CheckoutSummary';
+import EmptyCart from '../features/cart/EmptyCart';
+import { getTotalCartPrice } from '../features/Cart/cartSlice';
 
 function CartPage() {
+  const cart = useSelector((state) => state.cart.cart);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!cart.length) return <EmptyCart />;
+
   return (
-    <section className='section-px my-20 h-screen flex justify-center'>
-      <Cart />
-      <CheckoutSummary />
+    <section className='section-px my-20 h-screen flex justify-center gap-20'>
+      <Cart cart={cart} />
+      <CheckoutSummary totalCartPrice={totalCartPrice} />
     </section>
   );
 }
