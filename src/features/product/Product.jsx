39ee21525f052baa40c3dd90af51918e03513c87
yuki <38411009+yukijina@users.noreply.github.com>
2026-08-31 14:47:29 -0700

    destructuring object,create handleToCard function and add new item to the cart

--- src/features/product/Product.jsx
+++ src/features/product/Product.jsx
@@ -1,23 +1,34 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../components/Button';
+import { addItem } from '../Cart/cartSlice';
 
 function Product({ soap }) {
+  const { id, title, image, category, price } = soap;
+  const dispatch = useDispatch();
+
+  function handleAddToCart() {
+    const newItem = {
+      id: id,
+      itemName: title,
+      quantity: 1,
+      unitPrice: price,
+      totalPrice: price * 1,
+      image,
+    };
+    dispatch(addItem(newItem));
+  }
   return (
     <div className='bg-orange-100 rounded-2xl py-5 px-3 w-100 h-120'>
       <div className='flex justify-center pb-12'>
-        <img
-          src={soap.image}
-          alt={soap.title}
-          className='size-60 object-contain'
-        />
+        <img src={image} alt={title} className='size-60 object-contain' />
       </div>
-      <h5>{soap.title}</h5>
-      <p className='text-stone-500 pb-3'>{soap.category}</p>
-      <p className='font-semibold'>US$ {soap.price}</p>
-      <div>
-        <Button to='#' type='dark'>
-          Add to Cart
-        </Button>
-      </div>
+      <h5>{title}</h5>
+      <p className='text-stone-500 mb-3'>{category}</p>
+      <p className='font-semibold mb-2'>US$ {price}</p>
+
+      <Button type='dark' onClick={handleAddToCart}>
+        Add to Cart
+      </Button>
     </div>
   );
 }
