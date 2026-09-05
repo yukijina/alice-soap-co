712a22dc98c3a4ce49ad7c46bef30615b97b2f72
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-04 17:11:15 -0700

    create function for create order POST request

--- src/services/apiShop.js
+++ src/services/apiShop.js
@@ -1,10 +1,47 @@
-const API_URL = 'https://fakestoreapi.com/products';
+const API_URL = 'https://fakestoreapi.com';
 
 export async function getProducts() {
-  const res = await fetch(`${API_URL}?limit=20`);
+  const res = await fetch(`${API_URL}/products?limit=20`);
 
   if (!res.ok) throw Error('Failed getting products');
 
   const data = await res.json();
   return data;
+}
+
+// export async function createOrder(newOrder) {
+export async function createOrder() {
+  // test data
+  const newOrder = [
+    {
+      id: 0,
+      userId: 0,
+      products: [
+        {
+          id: 0,
+          title: 'new soap',
+          price: 80,
+          description: 'order',
+          category: 'soap',
+          image: 'http://example.com',
+        },
+      ],
+    },
+  ];
+
+  try {
+    const res = await fetch(`${API_URL}/carts`, {
+      method: 'POST',
+      body: JSON.stringify(newOrder),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    console.log(res);
+    if (!res.ok) throw Error();
+    const { data } = await res.json();
+    return data;
+  } catch (err) {
+    throw Error(`${err} - Failed creating your order`);
+  }
 }
