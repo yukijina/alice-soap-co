b51aaa89bc2d68cad48e81c249bab6c891208d2c
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-07 08:32:16 -0700

    add contents, add back to home button

--- src/pages/OrderConfirmationPage.jsx
+++ src/pages/OrderConfirmationPage.jsx
@@ -1,7 +1,26 @@
+import ConfirmationImg from '../assets/orderConfirmation.png';
+import Button from '../components/Button';
+
 function OrderConfirmationPage() {
   return (
-    <div>
-      <h3>Order confirmed. Your order number is#123</h3>
+    <div className='section-px flex flex-col items-center justify-center gap-2 mb-20'>
+      <div>
+        <img src={ConfirmationImg} alt='order confirmed' />
+      </div>
+      <h3>Order confirmed</h3>
+      <p>Thank you for your order!</p>
+      <h4>
+        Your order confirmation number is{' '}
+        <span className='font-semibold'>12345.</span>
+      </h4>
+
+      <div>
+        <p>This page will be automaticallyredirect to Home.</p>
+        <p>Or you can go back to home page &darr;</p>
+      </div>
+      <Button to='/' type='dark'>
+        Go back to Home
+      </Button>
     </div>
   );
 }
