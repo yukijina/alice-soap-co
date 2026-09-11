f5aad81af37d3a487d3118d9adfcaf36ed0972ba
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-11 15:38:34 -0700

    miner change. add space

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,7 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
-console.log(supabaseUrl);
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey);
 
