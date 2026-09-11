d0a966a228b97c593dc7fca73585a68de4879d44
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-11 15:37:32 -0700

    create a function to get a single product from external api

--- src/services/apiProducts.js
+++ src/services/apiProducts.js
@@ -1,7 +1,6 @@
 import supabase from './supabase';
 
 export async function getProducts() {
-  console.log('here!');
   const { data, error } = await supabase.from('products').select('*');
 
   if (error) {
@@ -9,5 +8,23 @@ export async function getProducts() {
     throw new Error('Products could noe be loaded');
   }
   console.log(data);
+  return data;
+}
+
+export async function getProduct(itemId) {
+  console.log('here!');
+  console.log(itemId);
+  const { data, error } = await supabase
+    .from('products')
+    .select('*')
+    .eq('id', itemId)
+    .single();
+
+  if (error) {
+    console.log(error);
+    throw new Error('Product could not loaded');
+  }
+
+  console.log(`single ${data}`);
   return data;
 }
