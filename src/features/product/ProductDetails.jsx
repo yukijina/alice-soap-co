6b5a34d4f00e759c4e8f73a786d3a2711e6f9c2a
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-11 15:35:22 -0700

    create loader to get a single item. get data and display.

--- src/features/product/ProductDetails.jsx
+++ src/features/product/ProductDetails.jsx
@@ -1,21 +1,90 @@
-import MainImg from '../../assets/lavender.png';
+import { useLoaderData } from 'react-router-dom';
+import { getProduct } from '../../services/apiProducts';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Popup from '../../components/Popup';
 import Button from '../../components/Button';
+import { CiFaceSmile } from 'react-icons/ci';
+import {
+  addItem,
+  getCurrentQuantityById,
+  increaseItemQuantity,
+} from '../cart/cartSlice';
 
-function ProductDetails({ product }) {
+function ProductDetails() {
+  const product = useLoaderData();
+  const { id, name, description, scentNote, ingredients, image, price } =
+    product;
+  const dispatch = useDispatch();
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+  const isInCart = currentQuantity > 0;
+
+  function handleAddToCart() {
+    const newItem = {
+      itemId: id,
+      itemName: name,
+      quantity: 1,
+      unitPrice: price,
+      totalPrice: price * 1,
+      image,
+    };
+    dispatch(addItem(newItem));
+    setIsPopupOpen(true);
+
+    // Close modal in 1 second
+    setTimeout(() => {
+      setIsPopupOpen(false);
+    }, 1500);
+  }
+
+  function handleIncreaseQuantity() {
+    dispatch(increaseItemQuantity(id));
+    setIsPopupOpen(true);
+
+    // Close modal in 1 second
+    setTimeout(() => {
+      setIsPopupOpen(false);
+    }, 1500);
+  }
+
   return (
-    <section className='section-px'>
+    <section className='section-px flex my-10'>
       <div>
-        <img src={MainImg} alt='Main Image' />
+        <img src={image} alt={name} />
       </div>
 
       <div>
-        <h2>title</h2>
-        <p>$12.00</p>
-        <p>details</p>
-        <Button type='dark'>Add to cart</Button>
+        <h2>{name}</h2>
+        <p className='font-semibold'>${price}</p>
+        <p className='mt-5 mb-8 text-lime-800'>{description}</p>
+        <p className='mb-2'>Scent Note: {scentNote}</p>
+        <p className='mb-10'>Ingredients: {ingredients}</p>
+
+        <Button
+          type='dark'
+          onClick={isInCart ? handleIncreaseQuantity : handleAddToCart}
+        >
+          Add to Cart
+        </Button>
+
+        {/* Popup Modal for deleting item */}
+        <Popup isOpen={isPopupOpen} onClose={() => setIsPopupOpen(false)}>
+          <div className='flex items-center justify-center'>
+            <p>The item has been added to your cart</p>
+            &nbsp;
+            <CiFaceSmile />
+          </div>
+        </Popup>
       </div>
     </section>
   );
+}
+
+export async function loader({ params }) {
+  // console.log(params);
+  const item = await getProduct(params.productId);
+  return item;
 }
 
 export default ProductDetails;
