a522e7db40a139aa98dff3956997206948249833
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-13 14:43:34 -0700

    add disclaimer sentence

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -11,9 +11,18 @@ function CartPage() {
   if (!cart.length) return <EmptyCart />;
 
   return (
-    <section className='section-px my-20 flex justify-center gap-20'>
-      <Cart cart={cart} />
-      <CheckoutSummary totalCartPrice={totalCartPrice} />
+    <section className='section-px my-20'>
+      <div className='mb-10 flex justify-center'>
+        <p className='text-red-400 bg-red-50 px-5 py-3 '>
+          This website is a fictional portfolio project created for
+          demonstration purposes only. No products or services are available for
+          purchase.
+        </p>
+      </div>
+      <div className='flex justify-center gap-20'>
+        <Cart cart={cart} />
+        <CheckoutSummary totalCartPrice={totalCartPrice} />
+      </div>
     </section>
   );
 }
--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -3,9 +3,17 @@ import CheckoutSummary from '../features/checkout/CheckoutSummary';
 
 function CheckoutPage() {
   return (
-    <section className='section-px my-20 flex justify-center gap-20'>
-      <AddressForm />
-      <CheckoutSummary />
+    <section className='section-px my-20 '>
+      <div className='flex justify-center gap-20'>
+        <AddressForm />
+        <CheckoutSummary />
+      </div>
+      <div className='mt-10 flex justify-center'>
+        <p className='text-red-400 bg-red-50 px-5 py-3 '>
+          This website is a fictional portfolio project created for
+          demonstration purposes only. Actual order won't be placed.
+        </p>
+      </div>
     </section>
   );
 }
