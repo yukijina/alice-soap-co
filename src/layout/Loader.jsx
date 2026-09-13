76dd42ea32761f3f312d733dc0bff000c2bfbe95
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-13 14:52:01 -0700

    miner update

--- src/layout/Loader.jsx
+++ src/layout/Loader.jsx
@@ -1,8 +1,8 @@
 function Loader() {
   console.log('loading in Loader now');
   return (
-    <div className='absolute inset-0 z-10 flex items-center justify-center bg-amber-50'>
-      <span className='loader'></span>
+    <div className='absolute inset-0 flex items-center justify-center bg-amber-50'>
+      <div className='loader'></div>
     </div>
   );
 }
