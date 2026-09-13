1fe90a006fdacf19d8094f3ba5c0feb9e61f2d50
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-13 14:51:28 -0700

    add disclaimer sentence

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,3 +1,7 @@
+import { CiHeart } from 'react-icons/ci';
+import { Link } from 'react-router-dom';
+import { currentYear } from '../utils/helpers';
+
 function Footer() {
   return (
     <footer className='section-px py-20 bg-orange-100 '>
@@ -6,29 +10,34 @@ function Footer() {
           <img src='/logo.svg' alt='Alice Soap Co. logo' className='w-23' />
           <p className='text-lime-800 text-sm'>San Francisco</p>
           <p className='pt-5 text-sm tracking-wider'>
-            Sustainably formulating plant-based skincare and premium
-            <br />
-            cold-pressed soap bars in San Francisco since 2022. <br />
-            Dedicated to gentle rituals and clean coastal health.
+            This website is a fictional portfolio project created for
+            demonstration purposes only. <br />
+            It is not a real store and is not affiliated with any real company,
+            person, brand, or product. <br />
+            No products or services are available for purchase. <br />
+            Thank you for visiting and exploring my work
+            <CiHeart className='inline text-red-600' />
           </p>
         </section>
 
         <section className=' flex flex-col justify-end'>
           <h5>Discover More</h5>
-          <a
-            href='#'
+          <Link
+            to='/our-story'
             className='text-stone-500 text-sm block tracking-wider leading-relaxed'
           >
             Alice's story
-          </a>
+          </Link>
           <a
-            href='#'
+            href='https://www.instagram.com'
+            target='_blank'
             className='text-stone-500 text-sm block tracking-wider leading-relaxed'
           >
             Ingredients index
           </a>
           <a
-            href='#'
+            href='https://www.ferrybuildingmarketplace.com/farmers-market/'
+            target='_blank'
             className='text-stone-500 text-sm block tracking-wider leading-relaxed'
           >
             Farmer's Market
@@ -38,13 +47,15 @@ function Footer() {
         <section className=' flex flex-col justify-end'>
           <h5>Connect</h5>
           <a
-            href='#'
+            href='https://www.instagram.com'
+            target='_blank'
             className='text-stone-500 text-sm block tracking-wider leading-relaxed'
           >
             Instagram
           </a>
           <a
-            href='#'
+            href='https://www.pinterest.com'
+            target='_blank'
             className='text-stone-500 text-sm block tracking-wider leading-relaxed'
           >
             Pintarest
@@ -60,7 +71,7 @@ function Footer() {
       <hr className='mt-30 mb-5 text-lime-800 opacity-10' />
       <div className='flex justify-between text-stone-500 text-xs'>
         <p>
-          &copy; 2026 Alice Soap Co. Hand-poured with love in San Francisco. All
+          &copy; {currentYear} Hand-poured with love in San Francisco. All
           rights reserved.
         </p>
 
