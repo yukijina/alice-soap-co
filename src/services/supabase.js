820d7407385b27120fa8338b37531ff1e63b8056
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-14 15:06:17 -0700

    update key name and add error message

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabasePublishableKey);
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error('Missing Supabase environment variables.');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
