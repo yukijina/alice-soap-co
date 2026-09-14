c6d6ebff29f79973debf06b899f83136fa211676
yuki <38411009+yukijina@users.noreply.github.com>
2026-09-14 15:46:21 -0700

    removed orderNumber generation for now

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,2 @@
 //***  get current year for copy right ***//
 export const currentYear = new Date().getFullYear();
-
-//***  create random order number ***//
-export const orderNumber = Math.floor(Math.random() * 100000);
